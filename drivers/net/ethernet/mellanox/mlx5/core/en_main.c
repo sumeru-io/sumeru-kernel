119cdc06add6c5285a91fccf0b9ed518d3e1bd24
minhuw <wangmh15@gmail.com>
2025-04-20 04:06:34 +0000

    fix(cacheflow): print the size of page pool for debugging

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -947,9 +947,13 @@ static int mlx5e_alloc_rq(struct mlx5e_params *params,
 
 #ifdef CONFIG_NET_CACHEFLOW
 		if (is_cacheflow_enabled()) {
-			pr_info("mlx5e: RQ[%d]: cacheflow enabled\n", rq->ix);
 			pp_params.flags |= PP_FLAG_CACHEFLOW;
 			pp_params.pool_size = get_cacheflow_pool_size();
+			pr_info("mlx5e: RQ[%d]: cacheflow enabled, pool size = %u\n", rq->ix, 
+				pp_params.pool_size);
+		} else {
+			pr_info("mlx5e: RQ[%d]: cacheflow disabled, pool size = %u\n", rq->ix, 
+				pp_params.pool_size);
 		}
 #endif
 
